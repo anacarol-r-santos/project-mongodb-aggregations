7ac1fc3c5d026c05386ee11f8afc3b4d2625ae3a
Ana Carolina R Santos <kroline0693@gmail.com>
2021-10-05 02:08:47 -0300

    [ADD] desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,34 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracaoMinutos: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      total: { $sum: 1 },
+      duracaoMedia: {
+        $avg: "$duracaoMinutos",
+      },
+    },
+  },
+  {
+    $project:{
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+]).pretty();
