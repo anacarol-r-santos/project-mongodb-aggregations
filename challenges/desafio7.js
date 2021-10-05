457a8594218a24f92912c9ec8e9d1aab4e038017
Ana Carolina R Santos <kroline0693@gmail.com>
2021-10-04 22:53:00 -0300

    [ADD] desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+  {
+    $unwind: "$cast",
+  },
+  {
+    $match: {
+      languages: "English",
+    },
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaImdb: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project:{
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaImdb", 1] },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+]).pretty();
