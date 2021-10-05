61fd5454c342f282c7367684165121ad15d06405
Ana Carolina R Santos <kroline0693@gmail.com>
2021-10-05 01:59:47 -0300

    [ADD] desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,34 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00"),
+        $lte: ISODate("2016-03-10T23:59:59"),
+      },
+    },
+  },
+  {
+    $addFields: {
+      duracaoMinutos: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMedia: {
+        $avg: "$duracaoMinutos",
+      },
+    },
+  },
+  {
+    $project:{
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMedia" },
+    },
+  },
+]).pretty();
